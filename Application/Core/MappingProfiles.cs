431a02b658b594b36b32236b7807051d5f1e9489
namiqmamedov <namigmm@code.edu.az>
2023-07-29 16:56:52 +0400

    Updating Update Attendance

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -9,9 +9,11 @@ namespace Application.Core
         public MappingProfiles()
         {
             CreateMap<Activity, Activity>();
+            
             CreateMap<Activity, ActivityDto>()
                 .ForMember(d => d.HostUsername, o => o.MapFrom(s => s.Attendees
                 .FirstOrDefault(x=>x.IsHost).AppUser.UserName));
+
             CreateMap<ActivityAttendee, Profiles.Profile>()
                 .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName))
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.AppUser.UserName))
