8feb5267fff581ab331dada0371fa43066d3c21f
namiqmamedov <namigmm@code.edu.az>
2023-07-31 16:19:38 +0400

    Adding a join entity

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -31,7 +31,7 @@ namespace API.Extensions
            services.AddCors(opt => 
             {
                 opt.AddPolicy("CorsPolicy",policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:5173");
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowCredentials().WithOrigins("http://localhost:5173");
                 });
             });
 
