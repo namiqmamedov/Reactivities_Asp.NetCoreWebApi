e287b478c6bf0863f5fed0a8f8c987f09b18e62b
namiqmamedov <namigmm@code.edu.az>
2023-08-01 14:05:10 +0400

    Updated profiles

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -38,6 +38,14 @@ namespace Application.Core
                 .ForMember(d => d.Username, o => o.MapFrom(s => s.Author.UserName))
                 .ForMember(d => d.Displayname, o => o.MapFrom(s => s.Author.DisplayName))
                 .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Photos.FirstOrDefault(x => x.IsMain).Url));
+
+            CreateMap<ActivityAttendee, Profiles.UserActivityDto>()
+                .ForMember(d => d.Id, o => o.MapFrom(s => s.AcitivtyId))
+                .ForMember(d => d.Date, o => o.MapFrom(s => s.Activity.Date))
+                .ForMember(d => d.Title, o => o.MapFrom(s => s.Activity.Title))
+                .ForMember(d => d.Category, o => o.MapFrom(s => s.Activity.Category))
+                .ForMember(d => d.HostUsername, o => o.MapFrom(s => 
+                    s.Activity.Attendees.FirstOrDefault(x => x.IsHost).AppUser.UserName));
         }
     }
 }
