02271c1df02f0407c6326ac12b1f2475769854a2
namiqmamedov <namigmm@code.edu.az>
2023-08-02 00:14:19 +0400

    Updated security header

--- API/Program.cs
+++ API/Program.cs
@@ -22,13 +22,15 @@ var app = builder.Build();
 // Configure the HTTP request pipeline. 
 app.UseMiddleware<ExceptionMiddleware>();
 
+const string modernizrHash = "sha256-47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=";
+
 app.UseXContentTypeOptions();
 app.UseReferrerPolicy(opt => opt.NoReferrer());
 app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
 app.UseXfo(opt => opt.Deny());
 app.UseCsp(opt => opt 
     .BlockAllMixedContent()
-    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+    .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com",modernizrHash))
     .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
     .FormActions(s => s.Self())
     .FrameAncestors(s => s.Self())
